7de6626a983cfb15278ad0b407a5149ce59a93c3
preetham <preethamgp1@gmail.com>
2020-09-12 22:18:29 +0530

    code

---
+++ app/src/main/java/com/example/mynotes/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mynotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MyAdapter myAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
